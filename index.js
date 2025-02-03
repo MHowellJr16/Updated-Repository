848bf01c51f6e3810489749f211b259f86b135bb
Mark <mhowelljr16@gmail.com>
2025-02-03 07:40:55 -0500

    Feb. 3_Objects

--- index.js
+++ index.js
@@ -4,12 +4,22 @@
  */
 let userFirstName = 'Mark'
 let userLastName = 'Howell'
-let user = {  // Uses '{}' instead of '[]
+let users = [
+    {  // Uses '{}' instead of '[]
     username: 'David',
     email: 'MHowellJr16@gmail.com',
     discordID: 'Howell2567',
-    lessonsCompleted: [0,1]
-} 
-console.log(user.email); // How you ACCESS objects in "console"
-console.log(user.email[0]);
-console.log(user.lessonsCompleted.map(elem => elem *2)) // can MAP as well 
+    lessonsCompleted: [0, 1],
+    }, 
+    {
+    username: 'Mark',
+    email: 'MHowellJr16@gmail.com',
+    discordID: 'Howell2567',
+    lessonsCompleted: [0, 1, 2],
+    }
+];
+console.log(users); // How you ACCESS objects in "console"
+console.log(users[0])
+console.log(users) // can MAP as well 
+console.log(users[0]);
+
